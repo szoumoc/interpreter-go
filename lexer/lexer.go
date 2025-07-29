2458d3e6293dabe7a7d386c633ce5554fff23444
szoumoc <soumosaha4327@gmail.com>
2025-07-29 22:20:17 +0530

    Lexer is ready!

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -42,7 +42,13 @@ func (l *Lexer) NextToken() token.Token {
 	l.skipWhitespace()
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case '(':
 		tok = newToken(token.LPAREN, l.ch)
 	case ')':
@@ -54,7 +60,13 @@ func (l *Lexer) NextToken() token.Token {
 	case '-':
 		tok = newToken(token.MINUS, l.ch)
 	case '!':
-		tok = newToken(token.BANG, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			tok = token.Token{Type: token.NOT_EQ, Literal: string(ch) + string(l.ch)}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
 	case '/':
 		tok = newToken(token.SLASH, l.ch)
 	case '*':
@@ -130,4 +142,19 @@ func (l *Lexer) readNumber() string {
 
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9'
+}
+
+// peekChar() is really similar to readChar(), except that it doesn’t increment l.position and
+// l.readPosition. We only want to “peek” ahead in the input and not move around in it, so
+// we know what a call to readChar() would return. Most lexers and parser have such a “peek”
+// function that looks ahead and most of the time it only returns the immediately next character.
+// The difficulty of parsing different languages often comes down to how far you have to peek
+// ahead (or look backwards!) in the source code to make sense of it.
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
+	}
 }
